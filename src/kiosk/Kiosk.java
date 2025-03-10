86e4e2b44e007b00bc9d24b32b026cf2b4572c37
jirong22 <qkrwldus995@gmail.com>
2025-03-10 22:01:27 +0900

    키오스크 저장

---
+++ src/kiosk/Kiosk.java
@@ -0,0 +1,5 @@
+package kiosk;
+
+public class Kiosk {
+    //프로ㅡ램 순서 흐름 제어 담당
+}
---
+++ src/kiosk/Menu.java
@@ -0,0 +1,8 @@
+package kiosk;
+
+public class Menu {
+    //MenuItem 클래스를 관리하는 클래스
+    // 버거 음료 메뉴등 카테고리내 여러 아이템
+
+
+}
